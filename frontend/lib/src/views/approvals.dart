df14b4680b9fbc9855a54d09680715d2fa646911
Deaponn <bartek.sajo@interia.pl>
2023-09-22 20:07:23 +0200

    Add basic navigation

---
+++ frontend/lib/src/views/approvals.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Approvals extends StatelessWidget{
+  const Approvals({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Text("Approvals page", textAlign: TextAlign.center);
+  }
+}
---
+++ frontend/lib/src/views/show_post.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShowPost extends StatelessWidget{
+  const ShowPost({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Text("ShowPost page", textAlign: TextAlign.center);
+  }
+}
